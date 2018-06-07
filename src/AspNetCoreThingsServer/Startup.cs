5f2a8436ca564689b384124e1f832122e7e5c9bb
damienbod <damien_bod@hotmail.com>
2018-06-07 17:21:52 +0200

    Updating API server to .NET Core 2.1

--- src/AspNetCoreThingsServer/Startup.cs
+++ src/AspNetCoreThingsServer/Startup.cs
@@ -42,7 +42,7 @@ namespace AspNetCoreThingsServer
 
             // Add framework services.
             services.AddSingleton<IThingsRepository, ThingsRepository>();
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
