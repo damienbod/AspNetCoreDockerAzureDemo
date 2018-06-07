1e3808d65c73f1ba06ec47651843f114f11ec1a5
damienbod <damien_bod@hotmail.com>
2018-06-07 19:49:50 +0200

    Updating to .NET core 2.1

--- src/AngularClient/Startup.cs
+++ src/AngularClient/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AngularClient
 {
--- src/AspNetCoreThingsServer/Startup.cs
+++ src/AspNetCoreThingsServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using AspNetCoreThingsServer.Repositories.Things;
 using AspNetCoreThingsServer.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreThingsServer
 {
